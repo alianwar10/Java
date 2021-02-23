f49a027678a5fa41a294732d776df402d1eb2977
Syed Anwar <hamzalianwar@hotmail.com>
2021-02-23 14:39:26 -0500

    First Challenge

---
+++ LbsToKilogramsChallenge.java
@@ -0,0 +1,8 @@
+public class LbsToKilogramsChallenge {
+    public static void main(String [] args) {
+
+        double numberOfPounds = 100;
+        double convertToKilograms = numberOfPounds * 0.45359237;
+        System.out.println(convertToKilograms);
+    }
+}
